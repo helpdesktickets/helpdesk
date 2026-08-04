6656911f8b7e5fe98bc8a021ec50e13ab3e4b366
helpdesktickets <ansanch.corp@gmail.com>
2026-08-04 12:44:29 -0700

    Add modal functionality for new ticket creation

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const newTicketBtn = document.getElementById("newTicketBtn");
+const modal = document.getElementById("modal");
+const cancel = document.getElementById("cancel");
 
+newTicketBtn.onclick = () => {
+    modal.classList.remove("hidden");
+};
+
+cancel.onclick = () => {
+    modal.classList.add("hidden");
+};
